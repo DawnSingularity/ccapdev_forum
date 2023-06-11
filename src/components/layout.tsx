2e45a0c84b68c8b22b6ca321335cb18a7ad10704
DawnSingularity <70613207+DawnSingularity@users.noreply.github.com>
2023-06-11 20:39:29 +0800

    git rid of some syntax

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 
 
-export const PageLayout = (props: PropsWithChildren<{}>)=>{
+export const PageLayout = (props: PropsWithChildren)=>{
     return(
         <div className="flex justify-center h-screen textColor">
           <div className="h-full w-full md:max-w-4xl border border-slate-500">
